bdc3a9663d798ff9a707d206eb16f63926f195f4
justino-dak <55016848+justino-dak@users.noreply.github.com>
2024-06-23 11:20:06 +0000

    ok

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/user");
+const path = require("path");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://dakustin:YedL2Eujq5V23qCx@cluster0.teikngd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+//routes
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
